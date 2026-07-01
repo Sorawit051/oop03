6186a8f4a01c3cd4c05e132fd522d469f21f78ae
Sorawit051 <sorawit051@users.noreply.github.com>
2026-07-01 11:57:09 +0700

    ส่งงานในห้อง Workshop3.1

---
+++ Workshop3_1.ts
@@ -0,0 +1,11 @@
+//function caiculateArea(width: number, height: number): number {
+//    return width * height;
+//}
+
+//let area:number = caiculateArea(5, 10);
+
+const caiculateArea = (width: number, height: number): number => {
+    return width * height;
+}
+
+console.log(`Area: ${caiculateArea(5, 10)}`);
